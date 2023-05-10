c04c7b236b2cd35048fca8c95ff95a9f65f0c632
AB-ohi <askohi63907@gmail.com>
2023-05-10 19:25:51 +0600

    db data get

--- env.js
+++
@@ -1,2 +0,0 @@
-//DB_USER = "coffeeShop"
-//DB_PASS = "3EZxQv96a15ZVXuN"
--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qthn2pl.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -24,9 +23,19 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
 
+    const coffeeCollection = client.db('coffeeDB').collection('coffee') 
+
     app.post('/coffee',async(req,res)=>{
       const newCoffee = req.body;
       console.log(newCoffee)
+      const result = await coffeeCollection.insertOne(newCoffee)
+      res.send(result);
+    })
+
+    app.get('/coffee', async(req, res)=>{
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     })
 
 
@@ -36,7 +45,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
